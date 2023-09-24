5abf3ea2148803e13aa9ad67664559adeab2bd81
baderanaas <anas.bader@insat.ucar.tn>
2023-09-24 15:16:58 +0100

    adding fps

--- FingerCountingProject.py
+++ FingerCountingProject.py
@@ -15,7 +15,20 @@ for imPath in myList:
     image = cv2.imread(f"{folderPath}/{imPath}")
     overlayList.append(image)
 
+pTime = 0
 while True:
     success, img = cap.read()
+
+    h, w, c = overlayList[0].shape
+    img[0:h, 0:w] = overlayList[0]
+
+    cTime = time.time()
+    fps = 1 / (cTime - pTime)
+    pTime = cTime
+
+    cv2.putText(
+        img, f"FPS: {int(fps)}", (400, 70), cv2.FONT_HERSHEY_PLAIN, 3, (255, 0, 0), 3
+    )
+
     cv2.imshow("Image", img)
     cv2.waitKey(1)
