d2211b32dbcf86e229f3eca26d69e3b0627c7cdd
baderanaas <anas.bader@insat.ucar.tn>
2023-09-24 15:09:18 +0100

    adding the images to the code and saving them in a list

--- FingerCountingProject.py
+++ FingerCountingProject.py
@@ -8,8 +8,14 @@ cap = cv2.VideoCapture(0)
 cap.set(3, wCam)
 cap.set(4, hCam)
 
+folderPath = "images"
+myList = os.listdir(folderPath)
+overlayList = []
+for imPath in myList:
+    image = cv2.imread(f"{folderPath}/{imPath}")
+    overlayList.append(image)
+
 while True:
-    
     success, img = cap.read()
     cv2.imshow("Image", img)
     cv2.waitKey(1)
